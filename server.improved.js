ce3d4e4fe1a2d42edba825c7a51c98b6db0e344a
christopheryon <203912328+christopheryon@users.noreply.github.com>
2025-09-19 15:07:53 -0400

    Proof of concept for GitHub OAuth

--- server.improved.js
+++ server.improved.js
@@ -1,11 +1,44 @@
 port = 3000
 const express = require('express')
 const app = express()
-app.use(express.static('public'))
 app.use(express.json());
+const session = require('express-session');
 require('dotenv').config({quiet: true})
+const passport = require('passport');
+const session_secret = process.env.EXPRESS_SESSION_SECRET
+app.use(session({secret: session_secret, resave: false, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session({}));
+const GitHubStrategy = require('passport-github2').Strategy;
+const github_client = process.env.GITHUB_CLIENT_ID
+const github_secret = process.env.GITHUB_CLIENT_SECRET
 
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.use(new GitHubStrategy({
+    clientID: github_client, clientSecret: github_secret, callbackURL: "http://localhost:3000/auth/github/callback"
+}, function (accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+        // use GitHub profile ID as user ID
+        return done(null, profile);
+    });
+}));
+const ensureAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login')
+}
+app.get('/', ensureAuthenticated)
+app.use(express.static('public'))
 const {MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
+const {join} = require("node:path");
 const user = process.env.DB_USER
 const pass = process.env.DB_PASSWORD
 const url = process.env.DB_URL
@@ -14,9 +47,7 @@ const uri = `mongodb+srv://${user}:${pass}@${url}/?retryWrites=true&w=majority&a
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
+        version: ServerApiVersion.v1, strict: true, deprecationErrors: true,
     }
 });
 const passwordEntries = client.db("password_records").collection("entries")
@@ -62,8 +93,26 @@ const calculateStrength = (password) => {
     }
 }
 
+app.get('/auth/github', passport.authenticate('github', {scope: ['user:email']}));
 
+app.get('/auth/github/callback', passport.authenticate('github', {failureRedirect: '/login'}), function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+});
+
+app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+
+app.get('/login', (req, res) => {
+    res.sendFile(join(__dirname, 'public', 'login.html'));
+});
 app.get('/passwords', async (req, res) => {
+    if (req.user) {
+        console.log("req user for passwords: " + JSON.stringify(req.user.id))
+    }
     let response = []
     const entryCursor = await passwordEntries.find()
     while (await entryCursor.hasNext()) {
